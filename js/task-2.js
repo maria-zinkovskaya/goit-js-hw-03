b5cc6c0d436f63077e3c0de2f910ee2a5ce94921
Maria Zinkovskaya <my.project.git@gmail.com>
2020-05-04 22:07:51 +0300

    1-5 task done

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,14 @@
 const countProps = function(obj) {
-    // твой код
-  };
-  
-  /*
-   * Вызовы функции для проверки работоспособности твоей реализации.
-   */
-  console.log(countProps({})); // 0
-  
-  console.log(countProps({ name: 'Mango', age: 2 })); // 2
-  
-  console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+  const keys = Object.keys(obj);
+
+  return keys.length;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countProps({}));
+
+console.log(countProps({ name: 'Mango', age: 2 }));
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
---
+++ js/task-4.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-restricted-syntax */
+const countTotalSalary = function(employees) {
+  const values = Object.values(employees);
+  console.log(values);
+  let totalSalary = 0;
+
+  for (const value of values) {
+    totalSalary += value;
+  }
+
+  return totalSalary;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
